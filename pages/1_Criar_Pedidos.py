911ffe4593fcb0d674398ba30675177f6af0b666
Arthursjeff <arthur.rosa1710@gmail.com>
2026-01-27 15:01:58 -0300

    Update 1_Criar_Pedidos.py

--- pages/1_Criar_Pedidos.py
+++ pages/1_Criar_Pedidos.py
@@ -2,6 +2,7 @@ import streamlit as st
 from core import (
     init_session,
     inject_css,
+    gate_login,
     render_criar_pedido
 )
 
@@ -9,7 +10,7 @@ st.set_page_config(page_title="Criar Pedido", layout="wide")
 
 init_session()
 inject_css()
-gate_login()
+
 
 if not st.session_state.get("logado", False):
     st.switch_page("app.py")
